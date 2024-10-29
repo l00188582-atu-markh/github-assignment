74e61412d1bbaa8e4af8805f41ecb9fff4f73b07
Mark.Hempenstall <L00188582@atu.ie>
2024-10-29 13:41:27 +0000

    feat(Project) Added simple C# class to set and get the number of doors

---
+++ CarDoors-L00188582/CarDoors-L00188582/CarDoorCountProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDoors_L00188582
+{
+    public class CarDoorCountProvider
+    {
+        private int _numberOfDoors = 0;
+
+        public int GetDoors()
+        {
+            return _numberOfDoors;
+        }
+
+        public void SetDoors(int quantityToSet)
+        {
+            _numberOfDoors = quantityToSet;
+        }
+    }
+}

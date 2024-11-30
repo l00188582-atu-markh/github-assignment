bcb5ff229c506d23c7e0471c5a65acca2a7f0252
Mark.Hempenstall <L00188582@atu.ie>
2024-11-30 13:29:12 +0000

    feat (transmission): Add TransmissionProvider class

---
+++ CarDoors-L00188582/CarDoors-L00188582/TransmissionProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDoors_L00188582
+{
+    public class TransmissionProvider
+    {
+        private string _transmission;
+
+        public string GetTransmissionType()
+        {
+            return _transmission;
+        }
+
+        public void SetTransmissionType(string transmissionType)
+        {
+            _transmission = transmissionType;
+        }
+    }
+}
